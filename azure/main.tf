321bfe880a38da6054edce388eafa160afec3978
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-14 15:40:53 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,10 +1,6 @@
 provider "azurerm" {
   features {}
 
- ARM_CLIENT_ID="2f567ba8-5e4e-4e64-adbe-6207512c9394"
- ARM_CLIENT_SECRET="01z8Q~r5pQQUe~rKwOt-aEXE-QhgApywXa~tWcZ4"
- ARM_SUBSCRIPTION_ID="03eddb61-8398-483b-9327-8dc016e1af21"
- ARM_TENANT_ID="c10f8df0-e818-4423-b387-68ce113e39cc"
 }
 resource "azurerm_resource_group" "rg" {
   name     = "${var.cluster_name}-rg"
