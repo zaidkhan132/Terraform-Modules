6138de9e8bbf0aaa0a41ee874fa3353db0a2de7b
zaidkhan132 <zaidbinazeez435@gmail.com>
2025-04-14 11:18:54 +0530

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,7 +1,20 @@
-variable "cluster_name" {}
-variable "location" {}
-variable "resource_group" {}
-variable "num_nodes" {
-  type = number
+variable "cluster_name" {
+  description = "Cluster name for VM resources"
+  type        = string
+}
+ 
+variable "location" {
+  description = "Azure location"
+  type        = string
+}
+ 
+variable "resource_group" {
+  description = "Resource Group name"
+  type        = string
+}
+ 
+variable "node_count" {
+  description = "Number of VMs to create"
+  type        = number
 }
  
